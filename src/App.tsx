6da85056752289e2cb40d86ca35c4a1f68464fdf
Egor Dubovik <egorka54896@gmail.com>
2023-02-15 11:58:52 +0300

    init: add base files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header"></header>
+        <section>hello world!</section>
+      </div>
+    </>
+  );
+}
+
+export default App;
