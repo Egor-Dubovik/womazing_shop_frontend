144f1755aa3762d85b4924643638965f96c1df3f
Egor Dubovik <egorka54896@gmail.com>
2023-02-15 19:30:57 +0300

    feet: add UserStore

---
+++ src/store/UserStore.ts
@@ -0,0 +1,35 @@
+import { makeAutoObservable } from 'mobx';
+import { IUser } from 'types/user.inerface';
+
+// interface IUserStore {
+//   _isAuth: boolean;
+// }
+
+class UserStore {
+  _isAuth: boolean;
+  _user: IUser | Record<string, never>;
+
+  constructor() {
+    this._isAuth = true;
+    this._user = {};
+    makeAutoObservable(this);
+  }
+
+  setIsAuth(value: boolean): void {
+    this._isAuth = value;
+  }
+
+  setUser(user: IUser): void {
+    this._user = user;
+  }
+
+  get isAuth(): boolean {
+    return this._isAuth;
+  }
+
+  get user(): IUser | Record<string, never> {
+    return this._user;
+  }
+}
+
+export default UserStore;
