fd0bfd271339b1d1a438a633e1d51c7ded836544
Egor Dubovik <egorka54896@gmail.com>
2023-02-16 23:13:51 +0300

    feet: add component TypeBar

---
+++ src/components/TypeBar.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { Context } from 'index';
+import { observer } from 'mobx-react-lite';
+import { ListGroup } from 'react-bootstrap';
+
+const TypeBar = observer(() => {
+  const { product } = useContext(Context);
+  return (
+    <ListGroup>
+      {product.types.map((type) => (
+        <ListGroup.Item
+          key={type.id}
+          style={{ cursor: 'pointer' }}
+          active={type.id === product.selectedType.id}
+          onClick={() => product.setSelectedType(type)}
+        >
+          {type.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+});
+
+export default TypeBar;
