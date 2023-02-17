d0a944a12bc830f13d8a7dddb92a3a7df8ad724f
Egor Dubovik <egorka54896@gmail.com>
2023-02-17 18:59:05 +0300

    refactor: move getPrice to utils

---
+++ src/utils/product.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IProduct } from 'types/product.interface';
+
+export const getPrice = (product: IProduct) =>
+  product.price === product.discount_price ? (
+    <p>${product.price}</p>
+  ) : (
+    <>
+      <div style={{ textDecoration: 'line-through', marginRight: 10 }}>
+        ${product.discount_price}
+      </div>
+      <div>${product.price}</div>
+    </>
+  );
